abbc9a2ad19e58dc25ef7ca5f2184f9240615cea
Luminoid <14302010006@fudan.edu.cn>
2015-11-22 17:35:53 +0800

    Rod-cutting problem: choices output

--- Technique/src/DynamicProgramming/RodCutting.java
+++ Technique/src/DynamicProgramming/RodCutting.java
@@ -1,5 +1,8 @@
 package DynamicProgramming;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by Ethan on 15/11/17.
  * <p/>
@@ -12,22 +15,57 @@ package DynamicProgramming;
  * </ol>
  */
 public class RodCutting {
-    public static int bottomUpCutRod(int[] p, int n) {
-        int[] r = new int[n + 1];
+
+    public static class Solution {
+        int[] r;
+        int[] s;
+
+        Solution(int[] r, int[] s) {
+            this.r = new int[r.length];
+            this.s = new int[s.length];
+            System.arraycopy(r, 0, this.r, 0, r.length); // Deep copy
+            System.arraycopy(s, 0, this.s, 0, s.length);
+        }
+    }
+
+    public static Solution bottomUpCutRod(int[] p, int n) {
+        int[] r = new int[n + 1]; // optimal revenue
+        int[] s = new int[n + 1]; // optimal choice
         for (int i = 1; i <= n; i++) {
             int q = Integer.MIN_VALUE;
             for (int j = 1; j <= i; j++) {
-                q = Math.max(q, p[j - 1] + r[i - j]);
+                if (q < p[j - 1] + r[i - j]) {
+                    q = p[j - 1] + r[i - j];
+                    s[i] = j;
+                }
             }
             r[i] = q;
         }
-        return r[n];
+        return (new Solution(r, s));
+    }
+
+    public static void printCutRodSolution(int[] p, int n) {
+        Solution solution = bottomUpCutRod(p, n);
+        ArrayList<Integer> choice = new ArrayList<Integer>();
+        int tmp = n;
+        while (tmp > 0) {
+            choice.add(solution.s[tmp]);
+            tmp -= solution.s[tmp];
+        }
+        System.out.println("The optimal cut of length " + n + ": " + Arrays.toString(choice.toArray()));
     }
 
     public static void main(String[] args) {
         int[] p = {1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+
+        Solution solution = bottomUpCutRod(p, 10);
+        int[] r = solution.r;
+        int[] s = solution.s;
+        System.out.println("The optimal revenue is: " + Arrays.toString(r));
+        System.out.println("The optimal choices are: " + Arrays.toString(s));
+
         for (int i = 1; i <= 10; i++) {
-            System.out.println("The optimal revenue of length " + i + ": " + bottomUpCutRod(p, i));
+            printCutRodSolution(p, i);
         }
     }
 }
