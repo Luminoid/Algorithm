825809f923b5d2a7db8284f29385571c445bfeac
Luminoid <14302010006@fudan.edu.cn>
2015-11-22 16:14:25 +0800

    Rod-cutting problem

--- Technique/src/DynamicProgramming/RodCutting.java
+++ Technique/src/DynamicProgramming/RodCutting.java
@@ -5,20 +5,29 @@ package DynamicProgramming;
  * <p/>
  * Rod-cutting problem: Given a rod of length n inches and a table of prices p_i for i = 1, 2, ... , n, determine the
  * maximum revenue r_n obtainable by cutting up the rod and selling the pieces.
+ * Two equivalent ways to implement a dynamic-programming approach:
+ * <ol>
+ * <li>top-down with memoization</li>
+ * <li>bottom-up method</li>
+ * </ol>
  */
 public class RodCutting {
-    public static void bottomUpCutRod(int[] p, int n) {
-        int[] r = new int[n+1];
-        for (int i = 0; i < n; i++) {
+    public static int bottomUpCutRod(int[] p, int n) {
+        int[] r = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
             int q = Integer.MIN_VALUE;
-            for (int j = 0; j < i; j++) {
-                q = Math.max(q, p[i] + r[j - i]);
+            for (int j = 1; j <= i; j++) {
+                q = Math.max(q, p[j - 1] + r[i - j]);
             }
             r[i] = q;
         }
+        return r[n];
     }
 
     public static void main(String[] args) {
         int[] p = {1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("The optimal revenue: " + bottomUpCutRod(p, i));
+        }
     }
 }
