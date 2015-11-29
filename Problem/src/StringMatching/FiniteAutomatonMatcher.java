c223c1834d39edb2d94a46bfdcd1cd384182312c
Luminoid <14302010006@fudan.edu.cn>
2015-11-29 17:06:40 +0800

    The Knuth-Morris-Pratt algorithm.

--- Problem/src/StringMatching/FiniteAutomatonMatcher.java
+++ Problem/src/StringMatching/FiniteAutomatonMatcher.java
@@ -6,25 +6,24 @@ package StringMatching;
 public class FiniteAutomatonMatcher {
     private static int[][] computeTransitionFunction(String P, char[] alphabet) {
         int m = P.length();
-        int[][] transition = new int[m+1][alphabet.length];
+        int[][] transition = new int[m + 1][alphabet.length];
         for (int q = 0; q <= m; q++) {
-            for (char c : alphabet){
+            for (char c : alphabet) {
                 int k = Math.min(m, q + 1);
-                while (!(P.substring(0,q)+c).endsWith(P.substring(0,k))){
+                while (!(P.substring(0, q) + c).endsWith(P.substring(0, k))) {
                     k--;
                 }
-                transition[q][c- alphabet[0]] = k;
+                transition[q][c - alphabet[0]] = k;
             }
         }
         return transition;
     }
 
     /**
-     *
-     * @param T Input text
+     * @param T          Input text
      * @param transition Transition function
-     * @param m Accepting state
-     * @param alphabet Finite input alphabet
+     * @param m          Accepting state
+     * @param alphabet   Finite input alphabet
      */
     public static void finiteAutomatonMatcher(String T, int[][] transition, int m, char[] alphabet) {
         int n = T.length();
@@ -44,7 +43,7 @@ public class FiniteAutomatonMatcher {
     public static void main(String[] args) {
         String T = "abababacaba";
         String P = "aba";
-        char[] alphabet = {'a','b','c'};
+        char[] alphabet = {'a', 'b', 'c'};
         finiteAutomata(T, P, alphabet);
     }
 }
---
+++ Problem/src/StringMatching/KmpMatcher.java
@@ -0,0 +1,49 @@
+package StringMatching;
+
+/**
+ * Created by Ethan on 15/11/29.
+ * The Knuth-Morris-Pratt algorithm.
+ */
+public class KmpMatcher {
+    public static int[] computePrefixFunction(String P, char base) {
+        int m = P.length();
+        int[] prefix = new int[m];
+        int k = 0;
+        for (int q = 1; q < m; q++) {
+            while (k > 0 && (P.charAt(k) - base) != (P.charAt(q) - base)) {
+                k = prefix[k];
+            }
+            if ((P.charAt(k) - base) == (P.charAt(q) - base)) {
+                k++;
+            }
+            prefix[q] = k;
+        }
+        return prefix;
+    }
+
+
+    public static void KmpMatcher(String T, String P, char base) {
+        int n = T.length();
+        int m = P.length();
+        int[] prefix = computePrefixFunction(P, base); // prefix function
+        int q = 0; // number of characters matched
+        for (int i = 0; i < n; i++) { // scan the text from left to right
+            while (q > 0 && (P.charAt(q) - base) != (T.charAt(i) - base)) {
+                q = prefix[q - 1]; // next character does not match
+            }
+            if ((P.charAt(q) - base) == (T.charAt(i) - base)) {
+                q++; // next character matches
+            }
+            if (q == m) { // is all of P matched?
+                System.out.println("Pattern occurs with shift " + (i + 1 - m));
+                q = prefix[q - 1]; // look for the next match
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String T = "abababacaba";
+        String P = "aba";
+        KmpMatcher(T, P, 'a');
+    }
+}
